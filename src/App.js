92a1c1d0c6c058f18427d351b405aa831d99f078
Pranjal Mishra <pranjal.ug19.cs@nitp.ac.in>
2022-02-05 17:34:07 +0530

    meals being displayed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Meals />
+    </>
+  );
 }
 
 export default App;

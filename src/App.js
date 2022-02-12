8bf66f34ca95b146e89aead6391a901bb95d41b9
Pranjal Mishra <pranjal.ug19.cs@nitp.ac.in>
2022-02-12 21:24:27 +0530

    fetch put request syncs the data at firebase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -5,6 +6,18 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://react-course-http-reques-39802-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}

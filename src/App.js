ca9c6e5112d3e8b78953c1e1efbea6de8f3470a2
Pranjal Mishra <pranjal.ug19.cs@nitp.ac.in>
2022-02-13 17:11:10 +0530

    another snapshot in v5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import { Route, Routes } from "react-router-dom";
-import MainHeader from "./components/MainHeader";
-import ProductDetail from "./pages/ProductDetail";
-import Products from "./pages/Products";
+import { Route, Switch, Redirect } from "react-router-dom";
+
 import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Routes>
-          <Route path="/welcome" element={<Welcome />} />
-
-          <Route path="/products" exact element={<Products />} />
-
-          <Route path="/products/:productId" element={<ProductDetail />} />
-        </Routes>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
 }
 
 export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Products Component
+// our-domain.com/product-detail/a-book
